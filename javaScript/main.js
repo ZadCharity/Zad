ab05210bffa28ef0e806d67c5e478bc74258ef10
ZadCharity <142100921+ZadCharity@users.noreply.github.com>
2023-08-13 17:55:54 +0300

    Zad Charity V0.1 Beta
    Winter is coming.

---
+++ javaScript/main.js
@@ -0,0 +1,65 @@
+var i = 0;
+var images = [];
+var slideTime = 3000; // 3 seconds
+
+images[0] = 'img/background/01.jpg';
+images[1] = 'img/background/02.jpg';
+images[2] = 'img/background/03.jpg';
+
+function changePicture() {
+    document.getElementById ('home').style.backgroundImage = "url(" + images[i] + ")";
+
+    if (i < images.length - 1) {
+        i++;
+    } else {
+        i = 0;
+    }
+    setTimeout(changePicture, slideTime);
+}
+
+window.onload = changePicture;
+console.log('yahia')
+
+
+
+
+
+// search-box open close js code
+let navbar = document.querySelector(".navbar");
+let searchBox = document.querySelector(".search-box .bx-search");
+// let searchBoxCancel = document.querySelector(".search-box .bx-x");
+
+// searchBox.addEventListener("click", ()=>{
+//   navbar.classList.toggle("showInput");
+//   if(navbar.classList.contains("showInput")){
+//     searchBox.classList.replace("bx-search" ,"bx-x");
+//   }else {
+//     searchBox.classList.replace("bx-x" ,"bx-search");
+//   }
+// });
+
+// sidebar open close js code
+let navLinks = document.querySelector(".nav-links");
+let menuOpenBtn = document.querySelector(".navbar .bx-menu");
+let menuCloseBtn = document.querySelector(".nav-links .bx-x");
+menuOpenBtn.onclick = function() {
+navLinks.style.left = "0";
+}
+menuCloseBtn.onclick = function() {
+navLinks.style.left = "-100%";
+}
+
+
+// sidebar submenu open close js code
+let htmlcssArrow = document.querySelector(".htmlcss-arrow");
+htmlcssArrow.onclick = function() {
+ navLinks.classList.toggle("show1");
+}
+let moreArrow = document.querySelector(".more-arrow");
+moreArrow.onclick = function() {
+ navLinks.classList.toggle("show2");
+}
+let jsArrow = document.querySelector(".js-arrow");
+jsArrow.onclick = function() {
+ navLinks.classList.toggle("show3");
+}
